3b7fb9fb782b93073fb213ed5e1a79b826aaf0aa
andreisava <andreisava88@gmail.com>
2024-11-16 18:47:14 +0200

    ci: set up cd workflow

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { postsRoutes } from './routes/posts.js'
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+postsRoutes(app)
+
+app.get('/', (req, res) => {
+  res.send('Hello form Express!')
+})
+
+export { app }
